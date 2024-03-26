38e0a04a1483d2d93788dab30c77501d9cbae7a6
Benson Wathigo <bensonwathigo21@gmail.com>
2024-03-26 12:13:30 +0300

    Dart Assignment

---
+++ Dart_Control_Flow_Assignment.dart
@@ -0,0 +1,15 @@
+void main () {
+  int i = 10;
+
+  if (i > 10) {
+    print("Your number is greater than 10");
+  }else if (i < 10) {
+    print ("Your number is less than 10");
+
+  }else if (i == 10 ){
+    print("Your number is equal 10");
+
+  }else{
+    print("Can't be identified...");
+  }
+}
